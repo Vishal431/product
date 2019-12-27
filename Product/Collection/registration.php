f3c7f24e3f0e6c74f2439218a1ab99dc0d03b12c
vishal <vishalsojitra58@gmail.com>
2019-12-27 14:21:32 +0530

    first commit

--- Product/Collection/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Product_Collection',
-    __DIR__
-    );
-?>
